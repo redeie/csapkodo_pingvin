eda8e9edcf6370ee4f48053e7fd4ba3c57ed45f2
redeie <95575761+redeie@users.noreply.github.com>
2024-04-13 12:41:02 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+let z = 0
+led.enable(false)
+let x = 0
+let y = 130
+ModuleWorld_PWM.Servo(ModuleWorld_PWM.mwServoNum.P1, y)
 basic.forever(function () {
-	
+    while (ModuleWorld_Digital.Ultrasonic(ModuleWorld_Digital.mwDigitalNum.P2P3) < 30) {
+        for (let x = 0; x <= 45; x++) {
+            y = x + 130
+            z = Math.map(ModuleWorld_Digital.Ultrasonic(ModuleWorld_Digital.mwDigitalNum.P2P3), 0, 30, 0, 15)
+            ModuleWorld_PWM.Servo(ModuleWorld_PWM.mwServoNum.P1, y)
+            basic.pause(z)
+        }
+        for (let x = 0; x <= 45; x++) {
+            y = 175 - x
+            z = Math.map(ModuleWorld_Digital.Ultrasonic(ModuleWorld_Digital.mwDigitalNum.P2P3), 0, 30, 0, 15)
+        }
+        ModuleWorld_PWM.Servo(ModuleWorld_PWM.mwServoNum.P1, y)
+        basic.pause(z)
+    }
 })
